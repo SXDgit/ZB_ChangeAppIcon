262acd7e91758aa906cd7da09e1e608f6d9092c6
sangxiedong <Sangxiedong@jishubu-sangxiedong.local>
2018-12-03 17:29:33 +0800

    动态修改App图标

--- ZB_ChangeAppIcon/ViewController.m
+++ ZB_ChangeAppIcon/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSArray *_dataArray;
+}
 
 @end
 
@@ -16,9 +18,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _dataArray = @[@"AIcon", @"BIcon", @"CIcon", @"ALLIcon"];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"修改图标" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    button.frame = CGRectMake(100, 100, 100, 30);
+    [button addTarget:self action:@selector(buttonAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
+- (void)buttonAction {
+    NSString *data = _dataArray[arc4random() % _dataArray.count];
+    [self setAppIconWithName:data];
+}
+
+- (void)setAppIconWithName:(NSString *)iconName {
+    if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+        return;
+    }
+    if ([iconName isEqualToString:@""]) {
+        iconName = nil;
+    }
+    [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"更换app图标发生错误了 ： %@",error);
+        }
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
