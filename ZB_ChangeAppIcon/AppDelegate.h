ad81720eebe680546adde7e7b149447ef7a96728
sangxiedong <Sangxiedong@jishubu-sangxiedong.local>
2018-12-03 17:25:18 +0800

    Initial Commit

---
+++ ZB_ChangeAppIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZB_ChangeAppIcon
+//
+//  Created by Sangxiedong on 2018/12/3.
+//  Copyright © 2018年 ZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
